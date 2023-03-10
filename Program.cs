496f661252bd0fd8e70c21ca14ae4bdbf2fe8314
morozzsasha@mail.ru <morozzsasha@mail.ru>
2023-03-10 21:56:45 +0300

    Добавились задачи второго семинара

--- Program.cs
+++ Program.cs
@@ -81,7 +81,7 @@
 // Console.WriteLine("Второй цифры нет");
 // else if (num>9 && num<=100)
 //      {
-        
+
 // Console.WriteLine(divnum % 10);
 //      }
 // else if (num>100)
@@ -96,9 +96,93 @@
 
 
 
- 
+
+// Cеминар 2 10.03.2023
+
+
+// 1 версия задачи
+
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+// int num1 = num/100;
+// int num2 = num % 10;
+// Console.WriteLine($"{num1}{num2}");
+
+// 2 версия задачи (готова). Условие - вводим число любой разрядности и удаляем из него вторую цифру
+
+// int x = Convert.ToInt32(Console.ReadLine());
+// int result = (int)Math.Floor(Math.Log(x, 10) + 1);
+// int index = 3;
+// if (result > 1)
+// {
+
+//     int digit = x / (int)Math.Pow(10, result - 1);
+//     Console.Write(digit % 10);
+//     do
+//     {
+//         int digit2 = x / (int)Math.Pow(10, result - index);
+//         Console.Write(digit2 % 10);
+//         index++;
+//     }
+//     while (index <= result);
+
+// }
+
+// else Console.WriteLine("Sorry, dude");
+
+// Получаем любое число, выдаем сумму всех его цифр( НЕ ДОДЕЛАНА)
+// Console.WriteLine("Введите любое целое число");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int result = (int)Math.Floor(Math.Log(x, 10) + 1);
+// int index = 3;
+// int sum=0;
+// int digit = x / (int)Math.Pow(10, result - 1);
+// while (index<result)
+// {index++;
+// sum=+digit%10;
+// }
+// Console.Write(sum);
+
+// else Console.WriteLine("Sorry, dude");
 
 
 
 
+// Console.WriteLine("Введите первое  число");
+// int num1= Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе  число");
+// int num2= Convert.ToInt32(Console.ReadLine());
+// int num3= num1%num2;
+// if (num3==0)
+//      Console.Write("Кратно");
+// else 
+//     Console.Write($"Не кратно, остаток  {num3}");
 
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num%7 == 0 && num%23 == 0)
+//     Console.Write("Кратно");
+// else
+//     Console.Write("Не кратно");
+
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+// Console.WriteLine("Введите первое  число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе  число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1*num1 == num2 || num2*num2 == num1) Console.WriteLine("Да");
+// else Console.WriteLine("Нет");
